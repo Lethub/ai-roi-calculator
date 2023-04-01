9b33cd6094add234198e0cc0ca829813153c98a5
Lethub <92879654+Lethub@users.noreply.github.com>
2023-04-01 03:23:36 -0700

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,13 +1,25 @@
-document.getElementById("roi-calculator").addEventListener("submit", function(event) {
-    event.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+    const roiCalculatorForm = document.getElementById("roi-calculator");
+    const roiResult = document.getElementById("roi-result");
 
-    const annualRent = parseFloat(document.getElementById("annual-rent").value);
-    const currentExpenses = parseFloat(document.getElementById("current-expenses").value);
-    const softwareCost = parseFloat(document.getElementById("software-cost").value);
+    roiCalculatorForm.addEventListener("submit", function (event) {
+        event.preventDefault();
 
-    const savings = currentExpenses * 0.5; // Assuming your software can save 50% of property management expenses
-    const netIncome = annualRent - (currentExpenses - savings) - softwareCost;
-    const roi = (netIncome / softwareCost) * 100;
+        const annualRent = parseFloat(document.getElementById("annual-rent").value);
+        const currentExpenses = parseFloat(document.getElementById("current-expenses").value);
+        const softwareCost = parseFloat(document.getElementById("software-cost").value);
 
-    document.getElementById("roi-result").innerText = `Estimated ROI: ${roi.toFixed(2)}%`;
+        const roi = calculateROI(annualRent, currentExpenses, softwareCost);
+
+        roiResult.textContent = `The ROI of the AI leasing software is ${roi.toFixed(2)}%.`;
+    });
+
+    function calculateROI(annualRent, currentExpenses, softwareCost) {
+        const netIncomeBeforeSoftware = annualRent - currentExpenses;
+        const netIncomeAfterSoftware = annualRent - (currentExpenses + softwareCost);
+
+        const roi = (netIncomeAfterSoftware / netIncomeBeforeSoftware) * 100;
+
+        return roi;
+    }
 });
